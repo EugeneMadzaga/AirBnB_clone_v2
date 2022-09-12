d6b31333de012f6a2566f3392199c3a675ae3279
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-09-12 20:50:18 +0200

    fix 3-python_route file

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
+"""
+    setup 4 routes and start the app
+"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_route():
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """simple route"""
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
-def hbnb_route():
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ /hbnb route """
+    return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_route(text):
-    return "C {:s}".format(text).replace("_", " ")
+@app.route('/c/<text>', strict_slashes=False)
+def arg1(text):
+    """ /c route """
+    tmp = text.replace('_', ' ')
+    return 'C {}'.format(tmp)
 
 
-@app.route('/python/', defaults={'text': "is cool"})
-@app.route('/python/<text>')
-def python_route(text):
-    return "Python {:s}".format(text).replace("_", " ")
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def arg2(text):
+    """ /python and optional param """
+    tmp = text.replace('_', ' ')
+    return 'Python {}'.format(tmp)
 
 
-if __name__ == "__main__":
-    app.run(port=5000, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
