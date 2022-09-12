2f17a33dba1564ae337dcec3bb7321b9c3b7c8c1
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-09-12 20:26:46 +0200

    fix c_route file

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
+"""
+    basic route
+"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_route():
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ basic route """
+    return 'Hello HBNB!'
 
-
-@app.route('/hbnb')
-def hbnb_route():
-    return "HBNB"
-
-
-@app.route('/c/<text>')
-def c_route(text):
-    return "C {:s}".format(text).replace("_", " ")
-if __name__ == "__main__":
-    app.run(port=5000, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
